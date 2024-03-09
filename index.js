8a8b2de246fd4f7bf3307a1a4f7edc3a4f2e1c29
LCiZY <1443351569@qq.com>
2024-03-09 22:53:53 +0800

    rm log

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var wslre = new RegExp("/mnt/.+")
 
 
 function convertPath(path) {
-    console.log(path)
     // 检查路径是否是Windows格式
     if (path.match(windowsre)) {
                 // 转换Windows路径到WSL路径
@@ -15,10 +14,10 @@ function convertPath(path) {
     }
     // 检查路径是否是WSL格式
     else if (path.match(wslre)) {
-               path = path.substring(5)
-               var c = path.charAt(0)
-               path = (c + ":").toUpperCase() + path.substring(1)
-               path = path.replace(/\//g, "\\")
+            path = path.substring(5)
+            var c = path.charAt(0)
+            path = (c + ":").toUpperCase() + path.substring(1)
+            path = path.replace(/\//g, "\\")
     }
     return path;
 }
